90c1b9ef25ecd8dec5b8a10b0900284aed82ce87
Pro100-Adm <kirow.alesha@yandex.ru>
2016-11-17 22:50:21 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,17 +1,25 @@
 import xml.etree.ElementTree as ET
 tree = ET.parse('ip_ranges.xml')
 root = tree.getroot()
+control = 0
 while True:
-    x = []
     ip_correct = 0
     ip = raw_input('Enter IP to find a host: ')
     for i in range (0,len(ip.split('.'))):
         if len(ip.split('.'))==4 and ip.split('.')[i].isdigit():
             ip_correct += 1
     if ip_correct==4:
-        for i in range (0,len(ip.split('.'))):
+        x = []
+        for i in range (len(ip.split('.'))):
             x.append(ip.split('.')[i])
-        print x
         for child in root:
             for child in child:
-                print child.attrib['Subnet']
+                range_az= child.attrib['Subnet'].split('.')
+                match = 0
+                for j in range(len(range_az)):
+                    if x[j]==range_az[j]:
+                        match+=1
+                if match >= 3:
+                    print "Hoster is Microsoft Azure."
+                    
+                            
